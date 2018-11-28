aca448c8919d3dc3046021b8bfd7ede48e01a98e
matheusfelic <mbdf@cin.ufpe.br>
2018-11-28 16:49:14 -0300

    adicionando gráficos e novos resultados

--- bkp/create_model_txt.py
+++ bkp/create_model_txt.py
@@ -28,14 +28,14 @@ from smac.facade.smac_facade import SMAC
 def mean_absolute_percentage_error(y_true, y_pred): 
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
-def mean_log_error(y_true, y_pred):
-    return np.mean(np.abs((np.log(y_true)-np.log(y_pred))))
-
 def getStopWords():
     stopwords = ""
     with open("stopwords.txt", "r") as sw:
         stopwords = sw.read()
     return stopwords.split()
+
+def mean_log_error(y_true, y_pred):
+    return np.mean(np.abs((np.log(y_true)-np.log(y_pred))))
 
 # Create a dataframe with the four feature variables
 train_csv = pd.read_csv(sys.argv[1])
@@ -77,27 +77,13 @@ test_csv = test_csv[test_csv.operation != 0]
 #print(test_csv.count)
 
 #create the text column
-#train_csv['txt'] = train_txt
-#test_csv['txt'] = test_txt
+train_csv['txt'] = train_txt
+test_csv['txt'] = test_txt
 
 #vectorizing text
 vector = TfidfVectorizer(stop_words=getStopWords(), max_features=1000)
-X_txt_train = vector.fit_transform(train_txt).todense()
-X_txt_test = vector.transform(test_txt).todense()
-
-#create df with vectorizer
-train_txt_df = pd.DataFrame(X_txt_train, columns=vector.get_feature_names())
-test_txt_df = pd.DataFrame(X_txt_test, columns=vector.get_feature_names())
-
-#Joining the 2 DFs
-train_csv = pd.concat([train_csv, train_txt_df])
-test_csv = pd.concat([test_csv, test_txt_df])
-
-#colocando valores 0 em palavras que não estão nos documentos. Evitando NaN
-train_csv.fillna(0, inplace=True)
-test_csv.fillna(0, inplace=True)
-
-print(train_csv.count)
+X_txt_train = vector.fit_transform(train_csv['txt']).todense()
+X_txt_test = vector.transform(test_csv['txt']).todense()
 
 print("train vector size: " + str(X_txt_train.shape))
 print("test vector size: " + str(X_txt_test.shape))
@@ -112,10 +98,9 @@ y = train_csv['price']
 #print(y.count)
 y_test_csv = test_csv['price']
 
-train_sample = train_csv.sample(n=len(train_csv), random_state=2)
-print(train_sample)
+#train_sample = train_csv.sample(n=len(train_csv), random_state=2)
 
-#train_sample = X_txt_train
+train_sample = X_txt_train
 
 # Show the number of observations for the test and training dataframes
 print('Number of observations in the training data:', len(train_sample))
@@ -157,13 +142,11 @@ def rf_from_cfg(cfg, seed):
 
     def rmse(y, y_pred):
         return np.sqrt(np.mean((y_pred - y)**2))
-
-      
+        
     # Creating root mean square error for sklearns crossvalidation
     rmse_scorer = make_scorer(rmse, greater_is_better=False)
     #score = cross_val_score(rfr, train_sample.iloc[:,1:7].as_matrix(), train_sample.iloc[:,0].as_matrix(), cv=11, scoring=rmse_scorer)
-    score = cross_val_score(rfr, train_sample.iloc[:, train_sample.columns != 'price'].as_matrix(), train_sample.iloc[:,0].as_matrix(), cv=11, scoring=rmse_scorer)
-    #score = cross_val_score(rfr, train_sample, train_csv['price'], cv=11, scoring=rmse_scorer)
+    score = cross_val_score(rfr, train_sample, train_csv['price'], cv=11, scoring=rmse_scorer)
     return -1 * np.mean(score)  # Because cross_validation sign-flips the score
 
 print("Running hyperparameter optimization...")
@@ -246,63 +229,63 @@ regr = RandomForestRegressor(max_depth=incumbent._values['max_depth'],
         random_state=0, 
         bootstrap=incumbent._values['num_trees'], min_samples_split=incumbent._values['min_samples_to_split'], min_samples_leaf=incumbent._values['min_samples_in_leaf'], max_leaf_nodes=incumbent._values['max_leaf_nodes'])
 
-svm.fit(train_csv[features], y)
-xgboost.fit(train_csv[features], y)
-regr.fit(train_csv[features], y)
-""" regr.fit(X_txt_train, y)
+#svm.fit(train_csv[features], y)
+#xgboost.fit(train_csv[features], y)
+#regr.fit(train_csv[features], y)
+regr.fit(X_txt_train, y)
 svm.fit(X_txt_train, y)
 xgboost.fit(X_txt_train, y)
- """
+
 # Apply the Classifier we trained to the test data (which, remember, it has never seen before)
-regr.predict(test_csv[features])
-svm.predict(test_csv[features])
-xgboost.predict(test_csv[features])
-""" regr.predict(X_txt_test)
+#regr.predict(test_csv[features])
+#svm.predict(test_csv[features])
+#xgboost.predict(test_csv[features])
+regr.predict(X_txt_test)
 svm.predict(X_txt_test)
 xgboost.predict(X_txt_test)
- """
+
 #metrics for the csv RF
-r_mle_rf = mean_log_error(regr.predict(test_csv[features]), y_test_csv)
-r_mape_rf = mean_absolute_percentage_error(regr.predict(test_csv[features]), y_test_csv)
-r_mae_rf  = np.median(abs(regr.predict(test_csv[features])-y_test_csv)/y_test_csv)
-r_mse_rf = mean_squared_error(regr.predict(test_csv[features]), y_test_csv)
-r_msle_rf = mean_squared_log_error(regr.predict(test_csv[features]), y_test_csv)
-r_r2_rf = r2_score(regr.predict(test_csv[features]), y_test_csv)
+#r_mae_rf  = np.median(abs(regr.predict(test_csv[features])-y_test_csv)/y_test_csv)
+#r_mse_rf = mean_squared_error(regr.predict(test_csv[features]), y_test_csv)
+#r_msle_rf = mean_squared_log_error(regr.predict(test_csv[features]), y_test_csv)
+#r_r2_rf = r2_score(regr.predict(test_csv[features]), y_test_csv)
 
 #metrics for the csv SVM
-r_mle_svm = mean_log_error(svm.predict(test_csv[features]), y_test_csv)
-r_mape_svm = mean_absolute_percentage_error(svm.predict(test_csv[features]), y_test_csv)
-r_mae_svm  = np.median(abs(svm.predict(test_csv[features])-y_test_csv)/y_test_csv)
-r_mse_svm = mean_squared_error(svm.predict(test_csv[features]), y_test_csv)
-r_msle_svm = mean_squared_log_error(svm.predict(test_csv[features]), y_test_csv)
-r_r2_svm = r2_score(svm.predict(test_csv[features]), y_test_csv)
+#r_mae_svm  = np.median(abs(svm.predict(test_csv[features])-y_test_csv)/y_test_csv)
+#r_mse_svm = mean_squared_error(svm.predict(test_csv[features]), y_test_csv)
+#r_msle_svm = mean_squared_log_error(svm.predict(test_csv[features]), y_test_csv)
+#r_r2_svm = r2_score(svm.predict(test_csv[features]), y_test_csv)
 
 #metrics for the csv XGB
-r_mle_xgb = mean_log_error(xgboost.predict(test_csv[features]), y_test_csv)
-r_mape_xgb = mean_absolute_percentage_error(xgboost.predict(test_csv[features]), y_test_csv)
-r_mae_xgb  = np.median(abs(xgboost.predict(test_csv[features])-y_test_csv)/y_test_csv)
-r_mse_xgb = mean_squared_error(abs(xgboost.predict(test_csv[features])), y_test_csv)
-r_msle_xgb = mean_squared_log_error(abs(xgboost.predict(test_csv[features])), y_test_csv)
-r_r2_xgb = r2_score(abs(xgboost.predict(test_csv[features])), y_test_csv)
+#r_mae_xgb  = np.median(abs(xgboost.predict(test_csv[features])-y_test_csv)/y_test_csv)
+#r_mse_xgb = mean_squared_error(abs(xgboost.predict(test_csv[features])), y_test_csv)
+#r_msle_xgb = mean_squared_log_error(abs(xgboost.predict(test_csv[features])), y_test_csv)
+#r_r2_xgb = r2_score(abs(xgboost.predict(test_csv[features])), y_test_csv)
 
 #metrics for txt RF
-""" r_mae_rf = np.median(abs(regr.predict(X_txt_test)-y_test_csv)/y_test_csv)
+r_mle_rf = mean_log_error(regr.predict(X_txt_test), y_test_csv)
+r_mape_rf = mean_absolute_percentage_error(regr.predict(X_txt_test), y_test_csv)
+r_mae_rf = np.median(abs(regr.predict(X_txt_test)-y_test_csv)/y_test_csv)
 r_mse_rf = mean_squared_error(regr.predict(X_txt_test), y_test_csv)
 r_msle_rf = mean_squared_log_error(regr.predict(X_txt_test), y_test_csv)
 r_r2_rf = r2_score(regr.predict(X_txt_test), y_test_csv)
- """
+
 #metrics for txt SVM
-""" r_mae_svm = np.median(abs(svm.predict(X_txt_test)-y_test_csv)/y_test_csv)
+r_mle_svm = mean_log_error(svm.predict(X_txt_test), y_test_csv)
+r_mape_svm = mean_absolute_percentage_error(svm.predict(X_txt_test), y_test_csv)
+r_mae_svm = np.median(abs(svm.predict(X_txt_test)-y_test_csv)/y_test_csv)
 r_mse_svm = mean_squared_error(svm.predict(X_txt_test), y_test_csv)
 r_msle_svm = mean_squared_log_error(svm.predict(X_txt_test), y_test_csv)
 r_r2_svm = r2_score(svm.predict(X_txt_test), y_test_csv)
- """
+
 #metrics for txt XGB
-""" r_mae_xgb = np.median(abs(xgboost.predict(X_txt_test)-y_test_csv)/y_test_csv)
+r_mle_xgb = mean_log_error(xgboost.predict(X_txt_test), y_test_csv)
+r_mape_xgb = mean_absolute_percentage_error(xgboost.predict(X_txt_test), y_test_csv)
+r_mae_xgb = np.median(abs(xgboost.predict(X_txt_test)-y_test_csv)/y_test_csv)
 r_mse_xgb = mean_squared_error(abs(xgboost.predict(X_txt_test)), y_test_csv)
 r_msle_xgb = mean_squared_log_error(abs(xgboost.predict(X_txt_test)), y_test_csv)
 r_r2_xgb = r2_score(abs(xgboost.predict(X_txt_test)), y_test_csv)
- """
+
 #printing metrics
 #RF
 print("RF:")
@@ -356,16 +339,16 @@ print("Building the final model...")
 #result for csv
 result = pd.concat([train_csv,test_csv])
 #result for txt
-#result_txt = np.concatenate((X_txt_train, X_txt_test))
+result_txt = np.concatenate((X_txt_train, X_txt_test))
 y = result['price']
 #csv
-regr.fit(result[features], y)
-svm.fit(result[features], y)
-xgboost.fit(result[features], y)
+#regr.fit(result[features], y)
+#svm.fit(result[features], y)
+#xgboost.fit(result[features], y)
 #txt
-#regr.fit(result_txt, y)
-#svm.fit(result_txt, y)
-#xgboost.fit(result_txt, y)
+regr.fit(result_txt, y)
+svm.fit(result_txt, y)
+xgboost.fit(result_txt, y)
 
 joblib.dump(regr,city+"_rf.pkl")
 log_model(regr, "model_rf")
